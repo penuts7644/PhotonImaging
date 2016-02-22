d3e009c01f90f229ac531e42483c81f9584f8a2d
lscheffer <lscheffer@bin201.bin>
2016-02-22 15:05:41 +0100

    code samengevoegd

--- src/main/java/Photon_Image_Processor.java
+++ src/main/java/Photon_Image_Processor.java
@@ -7,6 +7,9 @@ import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
 import ij.gui.GenericDialog;
+import ij.measure.ResultsTable;
+import ij.plugin.filter.Analyzer;
+import ij.plugin.filter.MaximumFinder;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
 
@@ -45,7 +48,7 @@ public class Photon_Image_Processor implements PlugInFilter {
             return PlugInFilter.DONE;
         }
 
-        image = imp;
+        this.image = imp;
         return PlugInFilter.DOES_8G | PlugInFilter.DOES_16 | PlugInFilter.DOES_32 | PlugInFilter.DOES_RGB;
     }
 
@@ -60,112 +63,37 @@ public class Photon_Image_Processor implements PlugInFilter {
     @Override
     public void run(ImageProcessor ip) {
         // get width and height
-        width = ip.getWidth();
-        height = ip.getHeight();
-
-        if (showDialog()) {
-            process(ip);
-            image.updateAndDraw();
-        }
-    }
-
-    private boolean showDialog() {
+        this.width = ip.getWidth();
+        this.height = ip.getHeight();
+        
         GenericDialog gd = new GenericDialog("Photon Image Processor");
 
+       
+        MaximumFinder m = new MaximumFinder();
+        System.out.println(m.findMaxima(ip, 50.0, MaximumFinder.LIST, false));
+        
+        //Analyzer a = new Analyzer(ip);
+        ResultsTable r = Analyzer.getResultsTable();
+        float[] xCo = r.getColumn(0);
+        float[] yCo = r.getColumn(1);
+       
+        
+        for (int i = 0; i < r.size(); i++){
+            System.out.println(i + ": x = " + xCo[i] + ", y = " + yCo[i]);
+            gd.addStringField("x", xCo[i]+"");
+        }
+                       
         // default value is 0.00, 2 digits right of the decimal point
         gd.addNumericField("value", 0.00, 2);
         gd.addStringField("name", "John");
-
         gd.showDialog();
-        if (gd.wasCanceled()) {
-            return false;
-        }
 
-        // get entered values
-        value = gd.getNextNumber();
-        name = gd.getNextString();
-
-        return true;
+//        if (showDialog()) {
+//            process(ip);
+//            image.updateAndDraw();
+//        }
     }
 
-    /**
-     * Process an image.
-     *
-     * Please provide this method even if {@link ij.plugin.filter.PlugInFilter} does require it; the method
-     * {@link ij.plugin.filter.PlugInFilter#run(ij.process.ImageProcessor)} can only handle 2-dimensional data.
-     *
-     * If your plugin does not change the pixels in-place, make this method return the results and change the
-     * {@link #setup(java.lang.String, ij.ImagePlus)} method to return also the
-     * <i>DOES_NOTHING</i> flag.
-     *
-     * @param image the image (possible multi-dimensional)
-     */
-    public void process(ImagePlus image) {
-        // slice numbers start with 1 for historical reasons
-        for (int i = 1; i <= image.getStackSize(); i++) {
-            process(image.getStack().getProcessor(i));
-        }
-    }
-
-    // Select processing method depending on image type
-    public void process(ImageProcessor ip) {
-        int type = image.getType();
-        if (type == ImagePlus.GRAY8) {
-            process((byte[]) ip.getPixels());
-        } else if (type == ImagePlus.GRAY16) {
-            process((short[]) ip.getPixels());
-        } else if (type == ImagePlus.GRAY32) {
-            process((float[]) ip.getPixels());
-        } else if (type == ImagePlus.COLOR_RGB) {
-            process((int[]) ip.getPixels());
-        } else {
-            throw new RuntimeException("not supported");
-        }
-    }
-
-    // processing of GRAY8 images
-    public void process(byte[] pixels) {
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                // process each pixel of the line
-                // example: add 'number' to each pixel
-                pixels[x + y * width] += (byte) value;
-            }
-        }
-    }
-
-    // processing of GRAY16 images
-    public void process(short[] pixels) {
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                // process each pixel of the line
-                // example: add 'number' to each pixel
-                pixels[x + y * width] += (short) value;
-            }
-        }
-    }
-
-    // processing of GRAY32 images
-    public void process(float[] pixels) {
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                // process each pixel of the line
-                // example: add 'number' to each pixel
-                pixels[x + y * width] += (float) value;
-            }
-        }
-    }
-
-    // processing of COLOR_RGB images
-    public void process(int[] pixels) {
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                // process each pixel of the line
-                // example: add 'number' to each pixel
-                pixels[x + y * width] += (int) value;
-            }
-        }
-    }
 
     public void showAbout() {
         IJ.showMessage("About Photon Image Processor",
@@ -191,11 +119,116 @@ public class Photon_Image_Processor implements PlugInFilter {
         // start ImageJ
         new ImageJ();
 
-        // open the Clown sample
-        ImagePlus image = IJ.openImage("http://imagej.net/images/clown.jpg");
-        image.show();
+        // Open the image sequence
+        IJ.run("Image Sequence...", "open=/commons/student/2015-2016/Thema11/Thema11_LScheffer_WvanHelvoirt/SinglePhotonData");
+        ImagePlus image = IJ.getImage();
+        
+        // Only if you use new ImagePlus(path)
+        //image.show();
 
         // run the plugin
         IJ.runPlugIn(clazz.getName(), "");
     }
+
+    
 }
+
+
+
+//    private boolean showDialog() {
+//        GenericDialog gd = new GenericDialog("Photon Image Processor");
+//
+//        // default value is 0.00, 2 digits right of the decimal point
+//        gd.addNumericField("value", 0.00, 2);
+//        gd.addStringField("name", "John");
+//
+//        gd.showDialog();
+//        if (gd.wasCanceled()) {
+//            return false;
+//        }
+//
+//        // get entered values
+//        value = gd.getNextNumber();
+//        name = gd.getNextString();
+//
+//        return true;
+//    }
+
+    /**
+     * Process an image.
+     *
+     * Please provide this method even if {@link ij.plugin.filter.PlugInFilter} does require it; the method
+     * {@link ij.plugin.filter.PlugInFilter#run(ij.process.ImageProcessor)} can only handle 2-dimensional data.
+     *
+     * If your plugin does not change the pixels in-place, make this method return the results and change the
+     * {@link #setup(java.lang.String, ij.ImagePlus)} method to return also the
+     * <i>DOES_NOTHING</i> flag.
+     *
+     * @param image the image (possible multi-dimensional)
+     */
+//    public void process(ImagePlus image) {
+//        // slice numbers start with 1 for historical reasons
+//        for (int i = 1; i <= image.getStackSize(); i++) {
+//            process(image.getStack().getProcessor(i));
+//        }
+//    }
+//
+//    // Select processing method depending on image type
+//    public void process(ImageProcessor ip) {
+//        int type = image.getType();
+//        if (type == ImagePlus.GRAY8) {
+//            process((byte[]) ip.getPixels());
+//        } else if (type == ImagePlus.GRAY16) {
+//            process((short[]) ip.getPixels());
+//        } else if (type == ImagePlus.GRAY32) {
+//            process((float[]) ip.getPixels());
+//        } else if (type == ImagePlus.COLOR_RGB) {
+//            process((int[]) ip.getPixels());
+//        } else {
+//            throw new RuntimeException("not supported");
+//        }
+//    }
+//
+//    // processing of GRAY8 images
+//    public void process(byte[] pixels) {
+//        for (int y = 0; y < height; y++) {
+//            for (int x = 0; x < width; x++) {
+//                // process each pixel of the line
+//                // example: add 'number' to each pixel
+//                pixels[x + y * width] += (byte) value;
+//            }
+//        }
+//    }
+//
+//    // processing of GRAY16 images
+//    public void process(short[] pixels) {
+//        for (int y = 0; y < height; y++) {
+//            for (int x = 0; x < width; x++) {
+//                // process each pixel of the line
+//                // example: add 'number' to each pixel
+//                pixels[x + y * width] += (short) value;
+//            }
+//        }
+//    }
+//
+//    // processing of GRAY32 images
+//    public void process(float[] pixels) {
+//        for (int y = 0; y < height; y++) {
+//            for (int x = 0; x < width; x++) {
+//                // process each pixel of the line
+//                // example: add 'number' to each pixel
+//                pixels[x + y * width] += (float) value;
+//            }
+//        }
+//    }
+//
+//    // processing of COLOR_RGB images
+//    public void process(int[] pixels) {
+//        for (int y = 0; y < height; y++) {
+//            for (int x = 0; x < width; x++) {
+//                // process each pixel of the line
+//                // example: add 'number' to each pixel
+//                pixels[x + y * width] += (int) value;
+//            }
+//        }
+//    }
