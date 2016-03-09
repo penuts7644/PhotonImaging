b8b39d634dafa62f94df4376634b898589a0b557
lscheffer <lscheffer@bin302.bin>
2016-03-09 12:57:49 +0100

    exacte posities finder is "af" maar lijkt niet te werken...

--- src/main/java/Photon_Image_Processor.java
+++ src/main/java/Photon_Image_Processor.java
@@ -48,6 +48,7 @@ public class Photon_Image_Processor implements PlugInFilter {
     private final int halfPhotonOutlineSize = this.photonOutlineSize/2;
     
     private int photonCounter = 0;
+    private int imageCounter = 0;
 
     /**
      * Setup method as initializer.
@@ -90,18 +91,19 @@ public class Photon_Image_Processor implements PlugInFilter {
         // get width and height
 //        this.width = ip.getWidth();
 //        this.height = ip.getHeight();
+        this.imageCounter++;
         float[][] rawCoordinates;
         
         this.preprocessImages(ip);
         
         MaximumFinder maxFind = new MaximumFinder();
         rawCoordinates = this.findPhotons(ip, maxFind);
-        
-        //System.out.println("\n************\n" + rawCoordinates[0].length + " photons found\n************");
+        System.out.println("\n************\nimage" + this.imageCounter + ", " + rawCoordinates[0].length + " photons found\n************");
         for (int i = 0; i < rawCoordinates[0].length; i++){
             float x = rawCoordinates[0][i];
             float y = rawCoordinates[1][i];
-            this.findExactCoordinates(x, y, ip);
+//            this.findExactCoordinates(x, y, ip);
+            System.out.println("X: " + x + " Y:" + y);
         }
         
         this.addToPhotonCount(rawCoordinates);
@@ -171,9 +173,14 @@ public class Photon_Image_Processor implements PlugInFilter {
         ImagePlus photonImp = new ImagePlus("single photon " + this.photonCounter, ip.crop());
         ImageProcessor photonIp = photonImp.getProcessor();
         
-        photonIp.setAutoThreshold(AutoThresholder.Method.Mean, false, ImageProcessor.BLACK_AND_WHITE_LUT);
-//        photonImp.show();
-        photonImp.updateAndDraw();
+        ip.resetRoi();
+        ip.crop();
+        
+        photonIp.setAutoThreshold(AutoThresholder.Method.Triangle, false, ImageProcessor.BLACK_AND_WHITE_LUT);
+        photonImp.show();
+        photonImp.updateImage();
+        //photonImp.updateAndDraw();
+        
         
         MaximumFinder m = new MaximumFinder();
         m.findMaxima(photonIp, 10, MaximumFinder.LIST, true);
@@ -185,11 +192,13 @@ public class Photon_Image_Processor implements PlugInFilter {
         foundCoordinates[0] = (int)xCor;
         foundCoordinates[1] = (int)yCor;
         
+        
         // If one of the found coordinatepairs is just the original coordinates,
         // then they were right in the beginning, return them
         for (int i = 0; i < results.getCounter(); i++){
             if (results.getValue("X", i) == this.halfPhotonOutlineSize &&
                 results.getValue("Y", i) == this.halfPhotonOutlineSize){
+                photonImp.close();
                 return foundCoordinates;
             }
         }
@@ -197,11 +206,13 @@ public class Photon_Image_Processor implements PlugInFilter {
         // Coordinates are different from the original coordinates:
         if (results.getCounter() == 0){
             // 1. if there were no coordinates found, return the original coordinates
+            System.out.println("image " + this.imageCounter + "photon " + this.photonCounter + ": none found, coordinates: " + xCor + ", " + yCor);
             return foundCoordinates;
         } else if(results.getCounter() == 1){
             // 2. if there was one coordinatepair found, return this pair
             foundCoordinates[0] = leftBoundary + (int)results.getValue("X", 0);
             foundCoordinates[1] = topBoundary + (int)results.getValue("Y", 0);
+            System.out.println("image " + this.imageCounter + "photon " + this.photonCounter + ": different found, coordinates: " + xCor + ", " + yCor + ", " + (int)results.getValue("X", 0) + ", " + (int)results.getValue("Y", 0));
         } else {
             // 3. there were multiple coordinatepairs found, return the one closest to the center,
             // this is most likely to be the correct one
@@ -222,12 +233,8 @@ public class Photon_Image_Processor implements PlugInFilter {
                     distance = newDistance;
                 }     
             }
+            System.out.println(this.photonCounter + ": multiple found");
         }
-        
-        
-        
-        ip.resetRoi();
-        ip.crop();
         
         return foundCoordinates;
     }
@@ -236,140 +243,7 @@ public class Photon_Image_Processor implements PlugInFilter {
         return (float) Math.sqrt((originalX - newX) * (originalX - newX) 
                                + (originalY - newY) * (originalY - newY));
     }
-    
-    private void findExactCoordinates2(float xCor, float yCor, ImageProcessor processor) {
-        int halfPOS = this.photonOutlineSize / 2;
-        Roi photonRoi = new Roi((xCor - halfPOS), (yCor - halfPOS), this.photonOutlineSize, this.photonOutlineSize);
-
-        processor.setRoi(photonRoi);
-        ImagePlus photonImagePlus = new ImagePlus("single photon " + this.photonCounter, processor.crop());
-            //photonImagePlus.show();
-            //System.out.println(photonImagePlus.isThreshold());
-            //System.out.println(String.join(" - ", AutoThresholder.getMethods()));
-//        if (this.temporaryCounter < 3){
-//            photonImagePlus.show();
-//        }
-        
-        //IJ.setAutoThreshold(photonImagePlus, "Mean dark B&W");
-            
-            //photonImagePlus.getProcessor().setAutoThreshold(AutoThresholder.Method.Mean, true, 2);
-            
-        ImageProcessor photonProc = photonImagePlus.getProcessor();
-        
-        
-        photonProc.setAutoThreshold(AutoThresholder.Method.Mean, false, ImageProcessor.BLACK_AND_WHITE_LUT);
-        
-//        57: 3.0, 10.0
-//115: 17.0, 16.0
-//192: 16.0, 15.0
-//194: 4.0, 10.0
-        
-        photonImagePlus.show();
-        photonImagePlus.updateAndDraw();
-        
-        //System.out.println(photonImagePlus.isThreshold());
-
-        
-        MaximumFinder m = new MaximumFinder();
-        m.findMaxima(photonProc, 10, MaximumFinder.LIST, true);
-        
-        ResultsTable results = ResultsTable.getResultsTable();
-        //System.out.println(results.getValue("X", 0) + ", " + results.getValue("Y", 0));
-        
-        ResultsTable.getResultsWindow().close(false);
-        
-        this.photonCounter++;
-        
-        if (results.getCounter() < 1){
-            System.out.println(this.photonCounter + " has no coordinates");
-//            newCoordinates[0] = xCor;
-//            newCoordinates[1] = yCor;
-//            return newCoordinates;
-        }else if (results.getCounter() > 1){
-            // There are multiple maxima found
-            for (int i = 0; i < results.getCounter(); i++){
-                if (results.getValue("X", i) == 10 || results.getValue("Y", i) == 10){
-//                    newCoordinates[0] = xCor;
-//                    newCoordinates[1] = yCor;
-//                    return newCoordinates;
-                    System.out.println(this.photonCounter + " has multiple but one with 10 10");
-                    photonImagePlus.close();
-                    return;
-                }
-            }
-            photonImagePlus.show();
-            System.out.println(this.photonCounter + " has multiple non-10 coordinates");
-            return;
-        }else {
-//            // 
-//            newCoordinates[0] = (float) (xCor + results.getValue("X", 0) - 10);
-//            newCoordinates[1] = (float) (yCor + results.getValue("Y", 0) - 10);
-//            return newCoordinates;
-            photonImagePlus.close();
-            if (results.getValue("X", 0) == 10 || results.getValue("Y", 0) == 10){
-                System.out.println(this.photonCounter + " has one with 10 10");
-                photonImagePlus.close();
-                return;
-            } else {
-                System.out.println(this.photonCounter + " IS SHIFTED");
-                return;
-            }
-        }
-   
-        
-//        try{
-//            if (results.getColumn(0).length > 1){
-//                photonImagePlus.show();
-//                System.out.println(this.temporaryCounter + ": " + results.getValue("X", 0) + ", " + results.getValue("Y", 0) + ", meer dan 1");
-//            }else if (results.getValue("X", 0) != 10){
-//                photonImagePlus.show();
-//                System.out.println(this.temporaryCounter + ": " + results.getValue("X", 0) + ", " + results.getValue("Y", 0));
-//            } else {
-//                photonImagePlus.close();
-//            }
-//        } catch(IllegalArgumentException e){
-//            photonImagePlus.show();
-//            System.out.println(this.temporaryCounter + ":  geen results");
-//        }
-
-        //photonImagePlus.close();
-        
-            
-        // Set the processor back to normal
-        processor.resetRoi();
-        processor.crop();
-            
-        
-
-        
-        
-//                // 1. handle no results
-//        if (results.getCounter() < 1){
-//            System.out.println(this.temporaryCounter + " has no coordinates");
-//            newCoordinates[0] = xCor;
-//            newCoordinates[1] = yCor;
-//            return newCoordinates;
-//        } else if (results.getCounter() > 1){
-//            // There are multiple maxima found
-//            for (int i = 0; i < results.getCounter(); i++){
-//                if (results.getValue("X", i) == 10 || results.getValue("Y", i) == 10){
-//                    newCoordinates[0] = xCor;
-//                    newCoordinates[1] = yCor;
-//                    return newCoordinates;
-//                } else {
-//                    // HANDLE THIS
-//                    photonImagePlus.show();
-//                    System.out.println(this.temporaryCounter + " has multiple non-10 coordinates");
-//                    // dichtstbijzijnde vinden
-//                }
-//            }
-//        } else {
-//            // 
-//            newCoordinates[0] = (float) (xCor + results.getValue("X", 0) - 10);
-//            newCoordinates[1] = (float) (yCor + results.getValue("Y", 0) - 10);
-//            return newCoordinates;
-//        }
-    }
+ 
     
     private void addToPhotonCount(float[][] coordinates){
         for (int i = 0; i < coordinates[0].length; i++){
@@ -424,7 +298,7 @@ public class Photon_Image_Processor implements PlugInFilter {
 //        IJ.run("Image Sequence...", "open=/commons/student/2015-2016/Thema11/Thema11_LScheffer_WvanHelvoirt/kleinbeetjedata");
 //        IJ.run("Image Sequence...", "open=/commons/student/2015-2016/Thema11/Thema11_LScheffer_WvanHelvoirt/meerdaneenkleinbeetje");
 //        IJ.run("Image Sequence...", "open=/commons/student/2015-2016/Thema11/Thema11_LScheffer_WvanHelvoirt/SinglePhotonData");
-        IJ.run("Image Sequence...", "open=/home/lonneke/imagephotondata");
+//        IJ.run("Image Sequence...", "open=/home/lonneke/imagephotondata");
         ImagePlus image = IJ.getImage();
 
         // Only if you use new ImagePlus(path)
@@ -434,100 +308,3 @@ public class Photon_Image_Processor implements PlugInFilter {
     }
 
 }
-
-//    private boolean showDialog() {
-//        GenericDialog gd = new GenericDialog("Photon Image Processor");
-//
-//        // default value is 0.00, 2 digits right of the decimal point
-//        gd.addNumericField("value", 0.00, 2);
-//        gd.addStringField("name", "John");
-//
-//        gd.showDialog();
-//        if (gd.wasCanceled()) {
-//            return false;
-//        }
-//
-//        // get entered values
-//        value = gd.getNextNumber();
-//        name = gd.getNextString();
-//
-//        return true;
-//    }
-/**
- * Process an image.
- *
- * Please provide this method even if {@link ij.plugin.filter.PlugInFilter} does require it; the method
- * {@link ij.plugin.filter.PlugInFilter#run(ij.process.ImageProcessor)} can only handle 2-dimensional data.
- *
- * If your plugin does not change the pixels in-place, make this method return the results and change the
- * {@link #setup(java.lang.String, ij.ImagePlus)} method to return also the
- * <i>DOES_NOTHING</i> flag.
- *
- * @param image the image (possible multi-dimensional)
- */
-//    public void process(ImagePlus image) {
-//        // slice numbers start with 1 for historical reasons
-//        for (int i = 1; i <= image.getStackSize(); i++) {
-//            process(image.getStack().getProcessor(i));
-//        }
-//    }
-//
-//    // Select processing method depending on image type
-//    public void process(ImageProcessor ip) {
-//        int type = image.getType();
-//        if (type == ImagePlus.GRAY8) {
-//            process((byte[]) ip.getPixels());
-//        } else if (type == ImagePlus.GRAY16) {
-//            process((short[]) ip.getPixels());
-//        } else if (type == ImagePlus.GRAY32) {
-//            process((float[]) ip.getPixels());
-//        } else if (type == ImagePlus.COLOR_RGB) {
-//            process((int[]) ip.getPixels());
-//        } else {
-//            throw new RuntimeException("not supported");
-//        }
-//    }
-//
-//    // processing of GRAY8 images
-//    public void process(byte[] pixels) {
-//        for (int y = 0; y < height; y++) {
-//            for (int x = 0; x < width; x++) {
-//                // process each pixel of the line
-//                // example: add 'number' to each pixel
-//                pixels[x + y * width] += (byte) value;
-//            }
-//        }
-//    }
-//
-//    // processing of GRAY16 images
-//    public void process(short[] pixels) {
-//        for (int y = 0; y < height; y++) {
-//            for (int x = 0; x < width; x++) {
-//                // process each pixel of the line
-//                // example: add 'number' to each pixel
-//                pixels[x + y * width] += (short) value;
-//            }
-//        }
-//    }
-//
-//    // processing of GRAY32 images
-//    public void process(float[] pixels) {
-//        for (int y = 0; y < height; y++) {
-//            for (int x = 0; x < width; x++) {
-//                // process each pixel of the line
-//                // example: add 'number' to each pixel
-//                pixels[x + y * width] += (float) value;
-//            }
-//        }
-//    }
-//
-//    // processing of COLOR_RGB images
-//    public void process(int[] pixels) {
-//        for (int y = 0; y < height; y++) {
-//            for (int x = 0; x < width; x++) {
-//                // process each pixel of the line
-//                // example: add 'number' to each pixel
-//                pixels[x + y * width] += (int) value;
-//            }
-//        }
-//    }
